b74c8f545286819927e9371b262e4ac5aa0ecf3f
Eugene Valeyev <evgen.povt@gmail.com>
2017-12-17 19:45:43 +0300

    Configured Platform contract migration

--- migrations/4_deploy_platform.js
+++ migrations/4_deploy_platform.js
@@ -2,9 +2,11 @@ const EWillEscrow = artifacts.require("./EWillEscrow.sol");
 const EWillAccount = artifacts.require("./EWillAccount.sol");
 const EWillPlatform = artifacts.require("./EWillPlatform.sol");
 
-module.exports = function(deployer, network) {
+module.exports = async function(deployer, network) {
   if (network == 'development') {
-    throw Error('not implemented');
+    const escrow = await deployer.deploy(EWillEscrow);
+    const account = await deployer.deploy(EWillAccount);
+    const platform = await deployer.deploy(EWillPlatform);
   }
   else {
     throw Error('not implemented');
