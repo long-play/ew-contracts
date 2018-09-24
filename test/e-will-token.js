e248bfb06ee5b70a1081728c2ced8466a586ab68
Alexander_MobileUp <ar@mobileup.ru>
2018-09-24 17:58:35 +0300

    refactored the test e-will-token.js

--- test/e-will-token.js
+++ test/e-will-token.js
@@ -1,39 +1,38 @@
-const EWillToken = artifacts.require("EWillToken");
+const EWillToken = artifacts.require('EWillToken');
 const TestUtils = require('./test-utils.js');
 
-contract('EWillToken', function(accounts) {
-  const owner    = accounts[0];
-  const account1 = accounts[1];
+contract('EWillToken', function([owner, account1]) {
+
   const ewTokenSupply = 100000;
 
   let ewToken = null;
 
-  it("should have a correct name", async () => {
+  it('should have a correct name', async () => {
     ewToken = await EWillToken.new(ewTokenSupply);
     const name = await ewToken.name.call();
-    assert.equal(name, 'E-will Token', 'the token has the wrong name');
+    name.should.be.equal('E-will Token');
   });
 
-  it("should have a correct supply", async () => {
+  it('should have a correct supply', async () => {
     const totalSupply = await ewToken.totalSupply.call();
-    assert.equal(totalSupply.toString(), ewTokenSupply.toString(), 'the token has the wrong Total Supply');
+    totalSupply.toString().should.be.equal(ewTokenSupply.toString());
   });
 
-  it("should grant the creator with the total supply", async () => {
+  it('should grant the creator with the total supply', async () => {
     const totalSupply = await ewToken.totalSupply.call();
     const balance = await ewToken.balanceOf.call(owner);
-    assert.equal(totalSupply.toString(), balance.toString(), 'the owner has the wrong token amount');
+    totalSupply.toString().should.be.equal(balance.toString());
   });
 
-  it("should allow to transfer tokens", async () => {
+  it('should allow to transfer tokens', async () => {
     const transferAmout = 1000;
     const txResult = await ewToken.transfer(account1, transferAmout, { from: owner });
     txEvent = TestUtils.findEvent(txResult.logs, 'Transfer');
-    assert.equal(txEvent.args.from, owner, 'transfered from the wrong account');
-    assert.equal(txEvent.args.to, account1, 'transfered to the wrong account');
-    assert.equal(txEvent.args.value, transferAmout, 'transfered wrong amount of tokens');
+    txEvent.args.from.should.be.bignumber.equal(owner);
+    txEvent.args.to.should.be.bignumber.equal(account1);
+    txEvent.args.value.should.be.bignumber.equal(transferAmout);
 
     const balance = await ewToken.balanceOf.call(account1);
-    assert.equal(transferAmout.toString(), balance.toString(), 'the account1 has the wrong token amount');
+    transferAmout.toString().should.be.equal(balance.toString());
   });
 });
