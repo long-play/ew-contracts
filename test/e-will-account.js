e263e905aff6e29b0019a93a5eb15edb28b468b6
Alexander_MobileUp <ar@mobileup.ru>
2018-09-24 16:40:57 +0300

    refactored the test e-will-account.js

--- test/e-will-account.js
+++ test/e-will-account.js
@@ -1,87 +1,79 @@
-const EWillToken = artifacts.require("EWillToken");
-const EWillAccount = artifacts.require("EWillAccount");
+const EWillToken = artifacts.require('EWillToken');
+const EWillAccount = artifacts.require('EWillAccount');
 const TestUtils = require('./test-utils.js');
 
-contract('EWillAccount', function(accounts) {
-  const admin   = accounts[0];
-  const acc     = accounts[1];
-  const holder1 = accounts[2];
-  const holder2 = accounts[3];
+contract('EWillAccount', function([admin, acc, holder1, holder2]) {
 
   let ewAccount = null;
+  let txResult, txEvent;
 
-  it("should create token", async () => {
+  it('should create token', async () => {
     ewToken = await EWillToken.new(1.0e+21);
     ewToken.transfer(holder1, 15.0e+18);
   });
 
-  it("should have a correct name", async () => {
+  it('should have a correct name', async () => {
     ewAccount = await EWillAccount.new(ewToken.address, 10, acc);
     await ewToken.addMerchant(ewAccount.address);
 
     const name = await ewAccount.name.call();
-    assert.equal(name, 'E-will Account', 'the contract has the wrong name');
+    name.should.be.equal('E-will Account');
   });
 
-  it("should configure the contract", async () => {
-    let txResult;
+  it('should configure the contract', async () => {
     txResult = await ewAccount.setFinance(admin, { from: admin });
     txResult = await ewAccount.setMinParkingAmount(15, { from: admin });
 
     const minParkingAmount = await ewAccount.minParkingAmount.call();
-    assert.equal(minParkingAmount.toString(), '15000000000000000000', 'the contract has the wrong Minimal Parking Fund');
+    minParkingAmount.toString().should.be.equal('15000000000000000000');
   });
 
-  it("should get funds", async () => {
-    let txResult;
+  it('should get funds', async () => {
     // transfer tokens first
     txResult = await ewToken.transfer(ewAccount.address, 70.0e+18, { from: admin });
     txResult = await ewAccount.fund(0xdeadbeaf, 70.0e+18, { from: admin });
     txEvent = TestUtils.findEvent(txResult.logs, 'Funded');
-    assert.equal(txEvent.args.willId, 0xdeadbeaf, 'the fund was made for the wrong willId');
-    assert.equal(txEvent.args.amount, 70.0e+18, 'the fund was made for the wrong amount');
+    txEvent.args.willId.should.be.bignumber.equal(0xdeadbeaf);
+    txEvent.args.amount.should.be.bignumber.equal(70.0e+18);
   });
 
-  it("should not pay more than a half of the balance for operational expenses", async () => {
-    let txResult = null;
+  it('should not pay more than a half of the balance for operational expenses', async () => {
+    let isCaught = false;
     const amount = 40.0e+18;
-
     try {
       txResult = await ewAccount.payOperationalExpenses(amount, { from: admin });
+      txEvent = TestUtils.findEvent(txResult.logs, 'Withdrew');
     } catch (err) {
-      assert.isNotNull(err, 'the withdraw is not declined although should be');
+      isCaught = true;
     }
-    assert.isNull(txResult, 'the withdraw is not declined although should be');
+    isCaught.should.be.equal(true);
   });
 
-  it("should pay for operational expenses", async () => {
-    let txResult;
+  it('should pay for operational expenses', async () => {
     const amount = 30.0e+18;
     txResult = await ewAccount.payOperationalExpenses(amount, { from: admin });
-
     txEvent = TestUtils.findEvent(txResult.logs, 'Withdrew');
-    assert.equal(txEvent.args.amount, amount, 'withdrew the wrong amount');
+    txEvent.args.amount.should.be.bignumber.equal(amount);
   });
 
-  it("should not pay oftener than a once per 30 days for operational expenses", async () => {
-    let txResult = null;
+  it('should not pay oftener than a once per 30 days for operational expenses', async () => {
+    let isCaught = false;
     const amount = 10.0e+18;
-
     try {
       txResult = await ewAccount.payOperationalExpenses(amount, { from: admin });
+      txEvent = TestUtils.findEvent(txResult.logs, 'Withdrew');
     } catch (err) {
-      assert.isNotNull(err, 'the withdraw is not declined although should be');
+      isCaught = true;
     }
-    assert.isNull(txResult, 'the withdraw is not declined although should be');
+    isCaught.should.be.equal(true);
   });
 
-  it("should allow to park tokens", async () => {
+  it('should allow to park tokens', async () => {
   });
 
-  it("should reward tokenholders", async () => {
+  it('should reward tokenholders', async () => {
   });
 
-  it("should allow to unpark tokens", async () => {
+  it('should allow to unpark tokens', async () => {
   });
-
 });
