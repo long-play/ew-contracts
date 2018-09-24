3c846fb676ab932020ccd984baba59cebbf1d296
Alexander_MobileUp <ar@mobileup.ru>
2018-09-24 14:16:34 +0300

    refactored the test e-will-platform.js

--- test/e-will-platform.js
+++ test/e-will-platform.js
@@ -7,14 +7,8 @@ const keccak256 = require('js-sha3').keccak256;
 const BN = require('bn.js');
 const TestUtils = require('./test-utils.js');
 
-contract('EWillPlatform', function(accounts) {
-  const admin = accounts[0];
-  const user  = accounts[1];
-  const prov  = accounts[2];
-  const benf  = accounts[3];
-  const deleg = accounts[4];
+contract('EWillPlatform', function([admin, user, prov, benf, deleg]) {
 
-  const willId = (new BN(prov.slice(2), 16)).iushln(96).iadd(new BN(0x31111d, 16)).toString(10);
   const ewTokenSupply = 100.0e+21;
 
   const ProviderState = {
@@ -47,115 +41,137 @@ contract('EWillPlatform', function(accounts) {
   let ewAccount = null;
   let ewEscrow = null;
   let ewToken = null;
+  let txResult, txEvent;
 
-  it("should have a correct name", async () => {
-    ewToken = await EWillToken.new(ewTokenSupply);
-    ewEscrow = await EWillEscrow.new(ewToken.address, 70);
-    ewAccount = await EWillAccount.new(ewToken.address, 1000, admin);
-    ewFinance = await EWillFinance.new(PLATFORM_FEE / 2, ewAccount.address, ewEscrow.address, ewToken.address);
-    ewPlatform = await EWillPlatform.new(ewFinance.address, ewEscrow.address, admin);
+  describe("#configuration", () => {
+    it("should have a correct name", async () => {
+      ewToken = await EWillToken.new(ewTokenSupply);
+      ewEscrow = await EWillEscrow.new(ewToken.address, 70);
+      ewAccount = await EWillAccount.new(ewToken.address, 1000, admin);
+      ewFinance = await EWillFinance.new(PLATFORM_FEE / 2, ewAccount.address, ewEscrow.address, ewToken.address);
+      ewPlatform = await EWillPlatform.new(ewFinance.address, ewEscrow.address, admin);
 
-    await ewFinance.setPlatform(ewPlatform.address);
-    await ewAccount.setFinance(ewFinance.address);
-    await ewEscrow.setFinance(ewFinance.address);
-    await ewToken.transfer(user, 15.0e+18);
-    await ewToken.transfer(prov, 150.0e+18);
-    await ewToken.transfer(ewFinance.address, 5.0e+21);
+      await ewFinance.setPlatform(ewPlatform.address);
+      await ewAccount.setFinance(ewFinance.address);
+      await ewEscrow.setFinance(ewFinance.address);
+      await ewToken.transfer(user, 15.0e+18);
+      await ewToken.transfer(prov, 150.0e+18);
+      await ewToken.transfer(ewFinance.address, 5.0e+21);
 
-    const name = await ewPlatform.name.call();
-    assert.equal(name, 'E-will Platform', 'the contract has the wrong name');
+      const name = await ewPlatform.name.call();
+      name.should.be.equal('E-will Platform');
+    });
+
+    it("should configure the contract", async () => {
+      await ewToken.addMerchant(ewEscrow.address);
+      await ewToken.addMerchant(ewAccount.address);
+      await ewToken.addMerchant(ewFinance.address);
+
+      txResult = await ewFinance.setAnnaulPlatformFee(500, { from: admin });
+      // 1 ether == $1000, 1 EWILL == $100
+      txResult = await ewFinance.setExchangeRates(1.0e+14, 1.0e+13, { from: admin });
+
+      const annualPlatformFee = await ewFinance.annualPlatformFee.call();
+      annualPlatformFee.toString().should.be.equal('500');
+
+      txResult = await ewEscrow.register(1000, 0x0badfeed, deleg, { from: prov });
+      txResult = await ewEscrow.activateProvider(prov, ProviderState.Activated, { from: admin });
+      txResult = await ewEscrow.topup(75.0e+18, { from: prov });
+    });
   });
 
-  it("should configure the contract", async () => {
-    let txResult;
-    await ewToken.addMerchant(ewEscrow.address);
-    await ewToken.addMerchant(ewAccount.address);
-    await ewToken.addMerchant(ewFinance.address);
+  describe("#creation and claim of will", () => {
+    const willId = (new BN(prov.slice(2), 16)).iushln(96).iadd(new BN(0x31111d, 16)).toString(10);
 
-    txResult = await ewFinance.setAnnaulPlatformFee(500, { from: admin });
-    // 1 ether == $1000, 1 EWILL == $100
-    txResult = await ewFinance.setExchangeRates(1.0e+14, 1.0e+13, { from: admin });
+    it("should create a will", async () => {
+      let benHash = (new BN(benf.slice(2), 16)).toBuffer();
+      benHash = new BN(keccak256(benHash), 16);
 
-    const annualPlatformFee = await ewFinance.annualPlatformFee.call();
-    assert.equal(annualPlatformFee.toString(), '500', 'the contract has the wrong Annual Platform Fee');
+      txResult = await ewPlatform.createWill('Test will for EV', willId, 0x5108a9e, 2, benHash.toString(10), prov, '0' /*todo: referrer*/, { from: user, value: 20.0e+15 });
+      txEvent = TestUtils.findEvent(txResult.logs, 'WillCreated');
+      txEvent.args.willId.should.be.bignumber.equal(willId);
+      txEvent.args.owner.should.be.bignumber.equal(user);
+      txEvent.args.provider.should.be.bignumber.equal(prov);
+      txEvent = TestUtils.findEvent(txResult.logs, 'WillStateUpdated');
+      txEvent.args.willId.should.be.bignumber.equal(willId);
+      txEvent.args.owner.should.be.bignumber.equal(user);
+      txEvent.args.newState.should.be.bignumber.equal(WillState.Created);
 
-    txResult = await ewEscrow.register(1000, 0x0badfeed, deleg, { from: prov });
-    txResult = await ewEscrow.activateProvider(prov, ProviderState.Activated, { from: admin });
-    txResult = await ewEscrow.topup(75.0e+18, { from: prov });
+      const will = await ewPlatform.wills.call(willId);
+      will[11].should.be.equal('Test will for EV');
+    });
+
+    it("should activate the will", async () => {
+      txResult = await ewPlatform.activateWill(willId, { from: deleg });
+      txEvent = TestUtils.findEvent(txResult.logs, 'WillStateUpdated');
+      txEvent.args.willId.should.be.bignumber.equal(willId);
+      txEvent.args.owner.should.be.bignumber.equal(user);
+      txEvent.args.newState.should.be.bignumber.equal(WillState.Activated);
+    });
+
+    it("should apply the will", async () => {
+      txResult = await ewPlatform.applyWill(willId, 0xe4c6, { from: deleg });
+      txEvent = TestUtils.findEvent(txResult.logs, 'WillStateUpdated');
+      txEvent.args.willId.should.be.bignumber.equal(willId);
+      txEvent.args.owner.should.be.bignumber.equal(user);
+      txEvent.args.newState.should.be.bignumber.equal(WillState.Pending);
+    });
+
+    it("should claim the will", async () => {
+      txResult = await ewPlatform.claimWill(willId, { from: benf });
+      txEvent = TestUtils.findEvent(txResult.logs, 'WillStateUpdated');
+      txEvent.args.willId.should.be.bignumber.equal(willId);
+      txEvent.args.owner.should.be.bignumber.equal(user);
+      txEvent.args.newState.should.be.bignumber.equal(WillState.Claimed);
+    });
+
+    it("should not decline the will", async () => {
+      let isCaught = false;
+      try {
+        txResult = await ewPlatform.rejectWill(willId, { from: deleg });
+        txEvent = TestUtils.findEvent(txResult.logs, 'WillStateUpdated');
+      } catch (err) {
+        isCaught = true;
+      }
+      isCaught.should.be.equal(true);
+    });
   });
 
-  it("should create a will", async () => {
-    let txResult, txEvent;
+  describe("#delegate decline the will", () => {
+    const willId = (new BN(prov.slice(2), 16)).iushln(96).iadd(new BN(0x31111c, 16)).toString(10);
 
-    let benHash = (new BN(benf.slice(2), 16)).toBuffer();
-    benHash = new BN(keccak256(benHash), 16);
+    it("should create a will", async () => {
+      let benHash = (new BN(benf.slice(2), 16)).toBuffer();
+      benHash = new BN(keccak256(benHash), 16);
 
-    txResult = await ewPlatform.createWill('Test will for EV', willId, 0x5108a9e, 2, benHash.toString(10), prov, '0' /*todo: referrer*/, { from: user, value: 20.0e+15 });
-    txEvent = TestUtils.findEvent(txResult.logs, 'WillCreated');
-    assert.equal(txEvent.args.willId.toString(10).toString(10), willId, 'the will is created with the wrong ID');
-    assert.equal(txEvent.args.owner, user, 'the will is created for the wrong user');
-    assert.equal(txEvent.args.provider, prov, 'the will is created for the wrong provider');
+      txResult = await ewPlatform.createWill('Test will for EV', willId, 0x5108a9e, 2, benHash.toString(10), prov, '0' /*todo: referrer*/, { from: user, value: 20.0e+15 });
+      txEvent = TestUtils.findEvent(txResult.logs, 'WillCreated');
 
-    txEvent = TestUtils.findEvent(txResult.logs, 'WillStateUpdated');
-    assert.equal(txEvent.args.willId.toString(10).toString(10), willId, 'the will is created with the wrong ID');
-    assert.equal(txEvent.args.owner, user, 'the will is created for the wrong user');
-    assert.equal(txEvent.args.newState, WillState.Created, 'the will is created with the wrong state');
+      txEvent.args.willId.should.be.bignumber.equal(willId);
+      txEvent.args.owner.should.be.bignumber.equal(user);
+      txEvent.args.provider.should.be.bignumber.equal(prov);
+      txEvent = TestUtils.findEvent(txResult.logs, 'WillStateUpdated');
+      txEvent.args.willId.should.be.bignumber.equal(willId);
+      txEvent.args.owner.should.be.bignumber.equal(user);
+      txEvent.args.newState.should.be.bignumber.equal(WillState.Created);
 
-    const will = await ewPlatform.wills.call(willId);
-    assert.equal(will[11], 'Test will for EV', 'the will is created with the wrong title');
-  });
+    });
 
-  it("should activate the will", async () => {
-    let txResult, txEvent;
+    it("should activate the will", async () => {
+      txResult = await ewPlatform.activateWill(willId, { from: deleg });
+      txEvent = TestUtils.findEvent(txResult.logs, 'WillStateUpdated');
+      txEvent.args.willId.should.be.bignumber.equal(willId);
+      txEvent.args.owner.should.be.bignumber.equal(user);
+      txEvent.args.newState.should.be.bignumber.equal(WillState.Activated);
+    });
 
-    txResult = await ewPlatform.activateWill(willId, { from: deleg });
-    txEvent = TestUtils.findEvent(txResult.logs, 'WillStateUpdated');
-    assert.equal(txEvent.args.willId.toString(10), willId, 'the will is created with the wrong ID');
-    assert.equal(txEvent.args.owner, user, 'the will is created for the wrong user');
-    assert.equal(txEvent.args.newState, WillState.Activated, 'the will is activated with the wrong state');
-  });
-
-  it("should apply the will", async () => {
-    let txResult, txEvent;
-
-    txResult = await ewPlatform.applyWill(willId, 0xe4c6, { from: deleg });
-    txEvent = TestUtils.findEvent(txResult.logs, 'WillStateUpdated');
-    assert.equal(txEvent.args.willId.toString(10), willId, 'the will is created with the wrong ID');
-    assert.equal(txEvent.args.owner, user, 'the will is created for the wrong user');
-    assert.equal(txEvent.args.newState, WillState.Pending, 'the will is applied with the wrong state');
-  });
-
-  it("should claim the will", async () => {
-    let txResult, txEvent;
-
-    txResult = await ewPlatform.claimWill(willId, { from: benf });
-    txEvent = TestUtils.findEvent(txResult.logs, 'WillStateUpdated');
-    assert.equal(txEvent.args.willId.toString(10), willId, 'the will is created with the wrong ID');
-    assert.equal(txEvent.args.owner, user, 'the will is created for the wrong user');
-    assert.equal(txEvent.args.newState, WillState.Claimed, 'the will is claimed with the wrong state');
-  });
-
-  it("should not decline the will", async () => {
-    let txResult, txEvent;
-
-    try {
+    it("should decline the will", async () => {
+      await TestUtils.gotoFuture(2 * ONE_YEAR + 1);
       txResult = await ewPlatform.rejectWill(willId, { from: deleg });
       txEvent = TestUtils.findEvent(txResult.logs, 'WillStateUpdated');
-      assert.isNull(txEvent, 'the will declined although should not');
-    } catch (err) {
-      assert.isNotNull(err, 'the will declined although should not');
-    }
-  });
-
-  //todo: needs to create a new will with acvtive state
-  it("should decline the will", async () => {
-    let txResult, txEvent;
-
-    await TestUtils.gotoFuture(2 * ONE_YEAR + 1);
-    txResult = await ewPlatform.rejectWill(willId, { from: deleg });
-    txEvent = TestUtils.findEvent(txResult.logs, 'WillStateUpdated');
-    assert.equal(txEvent.args.willId.toString(10), willId, 'the will is created with the wrong ID');
-    assert.equal(txEvent.args.owner, user, 'the will is created for the wrong user');
-    assert.equal(txEvent.args.newState, WillState.Rejected, 'the will is claimed with the wrong state');
+      txEvent.args.willId.should.be.bignumber.equal(willId);
+      txEvent.args.owner.should.be.bignumber.equal(user);
+      txEvent.args.newState.should.be.bignumber.equal(WillState.Rejected);
+    });
   });
 });
