4e9f44b98a6c3013b66819b7634db54a19b12207
Alexander_MobileUp <ar@mobileup.ru>
2018-09-28 14:27:25 +0300

    refactoring test e-will-finance

--- test/e-will-finance.js
+++ test/e-will-finance.js
@@ -2,19 +2,13 @@ const EWillToken = artifacts.require("EWillToken");
 const EWillEscrow = artifacts.require("EWillEscrow");
 const EWillAccount = artifacts.require("EWillAccount");
 const EWillPlatform = artifacts.require("EWillPlatform");
-
+const EWillMarketing = artifacts.require('EWillMarketing')
 const EWillFinance = artifacts.require("EWillFinance");
 const keccak256 = require('js-sha3').keccak256;
 const BN = require('bn.js');
 const TestUtils = require('./test-utils.js');
 
-contract('EWillFinance', function(accounts) {
-  const admin = accounts[0];
-  const user  = accounts[1];
-  const prov  = accounts[2];
-  const benf  = accounts[3];
-  const plat  = accounts[4];
-  const deleg = accounts[5];
+contract('EWillFinance', function([admin, user, prov, benf, plat, deleg]) {
 
   const ProviderState = {
     None: 0,
@@ -33,51 +27,66 @@ contract('EWillFinance', function(accounts) {
     Declined: 5
   };
 
+  const ONE_YEAR     = 365 * 24 * 3600; // in seconds
   const TOKEN_SUPPLY = 100.0e+21;   // 100,000 EWILLs
   const PLATFORM_FEE = 1500;        // cents, $15
   const PROVIDER_FEE = 2000;        // cents, $20
-  const REFFERER_RWD = 10;          // %
   const RATE_TOKEN   = 1.0e+14;     // tokenweis per cent, 100 $/EWILL
   const RATE_ETHER   = 1.0e+13;     // weis per cent, 1000 $/Ether
   const EXCHG_FEE    = 5;           // %
+  const DISCOUNT = 200;             // 20%
+  const REWARD = 100;               // 10%
+  const PROVIDER_DEAFULT_DSC = 300; // 30%
+  const PROVIDER_SPECIFIC_DSC = 400;// 40%
 
   let ewFinance = null;
   let ewAccount = null;
   let ewEscrow = null;
   let ewToken = null;
+  let ewMarketing = null;
+  let txResult, txEvent;
 
   it("should have a correct name", async () => {
     ewToken = await EWillToken.new(TOKEN_SUPPLY);
     ewEscrow = await EWillEscrow.new(ewToken.address, 70);
     ewAccount = await EWillAccount.new(ewToken.address, 1000, admin);
     ewFinance = await EWillFinance.new(PLATFORM_FEE / 2, ewAccount.address, ewEscrow.address, ewToken.address);
+    ewMarketing = await EWillMarketing.new(ewFinance.address, admin, ewToken.address);
 
     await ewEscrow.setFinance(ewFinance.address);
     await ewAccount.setFinance(ewFinance.address);
     await ewFinance.setPlatform(plat);
+    await ewFinance.setMarketing(ewMarketing.address);
     await ewFinance.setOracle(admin);
     await ewToken.transfer(user, 150.0e+18);
     await ewToken.transfer(prov, 150.0e+18);
     await ewToken.transfer(plat, 100.0e+18);
     await ewToken.transfer(ewFinance.address, 15.0e+18);
+    await ewToken.transfer(ewMarketing.address, 200.0e+18);
 
     const name = await ewFinance.name.call();
-    assert.equal(name, 'E-will Finance', 'the contract has the wrong name');
+    name.should.be.equal('E-will Finance');
   });
 
   it("should configure the contract", async () => {
-    let txResult;
     await ewToken.addMerchant(ewEscrow.address);
     await ewToken.addMerchant(ewAccount.address);
     await ewToken.addMerchant(ewFinance.address);
+    await ewMarketing.addDiscount(user,
+                                  Date.now() / 1000,
+                                  Date.now() / 1000 + ONE_YEAR,
+                                  DISCOUNT,
+                                  REWARD,
+                                  [0x0,                     prov],
+                                  [PROVIDER_DEAFULT_DSC,    PROVIDER_SPECIFIC_DSC],
+                                  { from: admin });
 
-    txResult = await ewFinance.setReferrerDiscount(REFFERER_RWD, { from: admin });
     txResult = await ewFinance.setAnnaulPlatformFee(PLATFORM_FEE, { from: admin });
     txResult = await ewFinance.setExchangeRates(RATE_TOKEN, RATE_ETHER, { from: admin });
     txResult = await ewFinance.setExchangeFee(EXCHG_FEE, { from: admin });
 
     const annualPlatformFee = await ewFinance.platformFee.call(1);
-    assert.equal(annualPlatformFee.toString(), PLATFORM_FEE, 'the contract has the wrong Annual Platform Fee');
+    annualPlatformFee.should.be.bignumber.equal(PLATFORM_FEE);
 
     txResult = await ewEscrow.register(PROVIDER_FEE, 0x0badfeed, deleg, { from: prov });
     txResult = await ewEscrow.activateProvider(prov, ProviderState.Activated, { from: admin });
@@ -85,8 +94,6 @@ contract('EWillFinance', function(accounts) {
   });
 
   it("should charge", async () => {
-    let txResult, txEvent;
-
     const bPlatform = await ewToken.balanceOf(plat);
     const bFinance = await ewToken.balanceOf(ewFinance.address);
     const bAccount = await ewToken.balanceOf(ewAccount.address);
@@ -94,15 +101,17 @@ contract('EWillFinance', function(accounts) {
 
     txResult = await ewFinance.charge(plat, prov, 0x0, 1, 'tokens', { from: plat, value: 0 });
 
-    assert.equal(bPlatform - await ewToken.balanceOf(plat), (PLATFORM_FEE + PROVIDER_FEE) * RATE_TOKEN, '');
-    assert.equal(await ewToken.balanceOf(ewFinance.address) - bFinance, 0, '');
-    assert.equal(await ewToken.balanceOf(ewAccount.address) - bAccount, PLATFORM_FEE * RATE_TOKEN, '');
-    assert.equal(await ewToken.balanceOf(ewEscrow.address) - bEscrow, PROVIDER_FEE * RATE_TOKEN, '');
+    txResult = bPlatform - await ewToken.balanceOf(plat)
+    txResult.should.be.bignumber.equal((PLATFORM_FEE + PROVIDER_FEE) * RATE_TOKEN);
+    txResult = await ewToken.balanceOf(ewFinance.address) - bFinance;
+    txResult.should.be.bignumber.equal(0);
+    txResult = await ewToken.balanceOf(ewAccount.address) - bAccount;
+    txResult.should.be.bignumber.equal(PLATFORM_FEE * RATE_TOKEN);
+    txResult = await ewToken.balanceOf(ewEscrow.address) - bEscrow;
+    txResult.should.be.bignumber.equal(PROVIDER_FEE * RATE_TOKEN);
   });
 
   it("should charge with ethers", async () => {
-    let txResult, txEvent;
-
     const ETH_VALUE = 7.0e+15;
     const ePlatform = await TestUtils.getBalance(ewFinance.address);
     const bPlatform = await ewToken.balanceOf(plat);
@@ -112,53 +121,60 @@ contract('EWillFinance', function(accounts) {
 
     txResult = await ewFinance.charge(plat, prov, 0x0, 1, 'ethers', { from: plat, value: ETH_VALUE });
 
-    assert.equal(await TestUtils.getBalance(ewFinance.address) - ePlatform, ETH_VALUE, '');
-    assert.equal(bPlatform - await ewToken.balanceOf(plat), (PLATFORM_FEE + PROVIDER_FEE - ETH_VALUE / RATE_ETHER) * RATE_TOKEN, '');
-    assert.equal(bFinance - await ewToken.balanceOf(ewFinance.address), ETH_VALUE / RATE_ETHER * RATE_TOKEN, '');
-    assert.equal(await ewToken.balanceOf(ewAccount.address) - bAccount, PLATFORM_FEE * RATE_TOKEN, '');
-    assert.equal(await ewToken.balanceOf(ewEscrow.address) - bEscrow, PROVIDER_FEE * RATE_TOKEN, '');
+    txResult = await TestUtils.getBalance(ewFinance.address) - ePlatform;
+    txResult.should.be.bignumber.equal(ETH_VALUE);
+    txResult = bPlatform - await ewToken.balanceOf(plat);
+    txResult.should.be.bignumber.equal((PLATFORM_FEE + PROVIDER_FEE - ETH_VALUE / RATE_ETHER) * RATE_TOKEN);
+    txResult = bFinance - await ewToken.balanceOf(ewFinance.address);
+    txResult.should.be.bignumber.equal(ETH_VALUE / RATE_ETHER * RATE_TOKEN);
+    txResult = await ewToken.balanceOf(ewAccount.address) - bAccount;
+    txResult.should.be.bignumber.equal(PLATFORM_FEE * RATE_TOKEN);
+    txResult = await ewToken.balanceOf(ewEscrow.address) - bEscrow;
+    txResult.should.be.bignumber.equal(PROVIDER_FEE * RATE_TOKEN);
   });
 
-/*
-  todo: to make the testcase passing there needs to add a maketing contract
   it("should charge with referrer reward", async () => {
-    let txResult, txEvent;
-
+    const referrer = benf;
     const bReferrer = await ewToken.balanceOf(benf);
     const bPlatform = await ewToken.balanceOf(plat);
     const bFinance = await ewToken.balanceOf(ewFinance.address);
     const bAccount = await ewToken.balanceOf(ewAccount.address);
     const bEscrow = await ewToken.balanceOf(ewEscrow.address);
+    const bMarketing = await ewToken.balanceOf(ewMarketing.address);
 
-    txResult = await ewFinance.charge(plat, prov, benf, 1, 'referref', { from: plat, value: 0 });
+    txResult = await ewFinance.charge(plat, prov, referrer, 1, 'referrer', { from: plat, value: 0 });
 
-    const refReward = PLATFORM_FEE * REFFERER_RWD / 100;
-    assert.equal(await ewToken.balanceOf(benf) - bReferrer, refReward * RATE_TOKEN, '');
-    assert.equal(bPlatform - await ewToken.balanceOf(plat), (PLATFORM_FEE + PROVIDER_FEE - refReward) * RATE_TOKEN, '');
-    assert.equal(await ewToken.balanceOf(ewFinance.address) - bFinance, 0, '');
-    assert.equal(await ewToken.balanceOf(ewAccount.address) - bAccount, (PLATFORM_FEE - 2 * refReward) * RATE_TOKEN, '');
-    assert.equal(await ewToken.balanceOf(ewEscrow.address) - bEscrow, PROVIDER_FEE * RATE_TOKEN, '');
+    txResult = await ewToken.balanceOf(referrer) - bReferrer;
+    txResult.should.be.bignumber.equal((PLATFORM_FEE * REWARD / 1000) * RATE_TOKEN);    
+    bPlatform.should.be.bignumber.equal(await ewToken.balanceOf(plat));
+    txResult.should.be.bignumber.equal(ewFinance.address);
+    txResult = await ewToken.balanceOf(ewAccount.address) - bAccount;
+    txResult.should.be.bignumber.equal(PLATFORM_FEE * RATE_TOKEN);
+    txResult = await ewToken.balanceOf(ewEscrow.address) - bEscrow
+    txResult.should.be.bignumber.equal(PROVIDER_FEE * RATE_TOKEN);
+
+    txResult = bMarketing - await ewToken.balanceOf(ewMarketing.address);
+    txResult.should.be.bignumber.equal((PLATFORM_FEE * (REWARD + DISCOUNT) / 1000) * RATE_TOKEN);   
   });
-*/
+
 
   it("should reward a provider", async () => {
-    let txResult, txEvent;
-
     txResult = await ewFinance.reward(prov, 1500, 0, { from: plat });
   });
 
   it("should exchange the tokens", async () => {
-    let txResult, txEvent;
-
     const TKN_VALUE = 5.0e+15;
     const bUser = await ewToken.balanceOf(user);
     const bFinance = await ewToken.balanceOf(ewFinance.address);
     const eFinance = await TestUtils.getBalance(ewFinance.address);
 
     txResult = await ewFinance.exchangeTokens(TKN_VALUE, { from: user });
-    assert.equal(bUser - await ewToken.balanceOf(user), TKN_VALUE, '');
-    assert.equal(await ewToken.balanceOf(ewFinance.address) - bFinance, TKN_VALUE, '');
-    assert.equal(eFinance - await TestUtils.getBalance(ewFinance.address), TKN_VALUE * ((100 - EXCHG_FEE) / 100) * (RATE_ETHER / RATE_TOKEN), '');
-  });
 
+    txResult = bUser - await ewToken.balanceOf(user);
+    txResult.should.be.bignumber.equal(TKN_VALUE);
+    txResult = await ewToken.balanceOf(ewFinance.address) - bFinance;
+    txResult.should.be.bignumber.equal(TKN_VALUE);
+    txResult = eFinance - await TestUtils.getBalance(ewFinance.address);
+    txResult.should.be.bignumber.equal(TKN_VALUE * ((100 - EXCHG_FEE) / 100) * (RATE_ETHER / RATE_TOKEN));
+  });
 });
